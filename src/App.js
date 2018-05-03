bc05c91c52792847f1332a6d699e8d9e8a505c26
Daniel Sanchez <jose.sanchez@ironbit.com.mx>
2018-05-03 16:29:41 -0500

    trying to use a fucking react-scripts fork no success

--- src/App.js
+++ src/App.js
@@ -1,20 +1,61 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+// import { View, Text, TextInput, StyleSheet } from 'react-native'
 
-export default class App extends Component {
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
+export default class AnatomyExample extends Component {
   render() {
     return (
-      <View style={styles.app}>
-        <View style={styles.appHeader}>
-          <Text style={styles.appTitle}>Welcome to React <span role="img">⚛</span>️</Text>
-        </View>
-        <Text style={styles.appIntro}>
-          To get started, <code>src/App.js</code> and save to reload.
-        </Text>
-      </View>
-    )
+      <Container>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name='menu' />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Header</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Content>
+          <Text>
+            This is Content Section
+          </Text>
+        </Content>
+        <Footer>
+          <FooterTab>
+            <Button full>
+              <Text>Footer</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
+    );
   }
 }
+
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <View style={styles.app}>
+//         <View style={styles.appHeader}>
+//           <Text style={styles.appTitle}>Welcome to React <span role="img">⚛</span>️</Text>
+//         </View>
+//         <Text style={styles.appIntro}>
+//           To get started, <code>src/App.js</code> and save to reload.
+//         </Text>
+//         <View style={{flex: 3}}>
+//           <TextInput
+//             placeholder="Type here to start writing"
+//             multiline
+//             numberOfLines={20}
+//             underlineColorAndroid={"transparent"}
+//           />
+//         </View>
+//       </View>
+//     )
+//   }
+// }
 
 const styles = StyleSheet.create({
   app: {
